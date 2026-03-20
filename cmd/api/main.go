45f3657c4ed32477cb4d114dd279b258cfde0baf
ankur <ankurdwivedi.dce@gmail.com>
2026-03-20 17:21:42 +0530

    Server init

---
+++ cmd/api/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"net/http"
+)
+
+func main() {
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Get("/", func(writer http.ResponseWriter, request *http.Request) {
+		writer.Write([]byte("Hello World!"))
+	})
+	http.ListenAndServe(":3000", r)
+}
--- go.mod
+++ go.mod
@@ -1,1 +1,5 @@
 module github.com/ankurdubey28/atlas-platform
+
+go 1.25.8
+
+require github.com/go-chi/chi/v5 v5.2.5 // indirect
