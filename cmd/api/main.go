678679512e0e45bcb1e89214981dbce0cc85f583
ankur <ankurdwivedi.dce@gmail.com>
2026-03-21 22:35:20 +0530

    add env file

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,12 +1,23 @@
 package main
 
 import (
+	"fmt"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/joho/godotenv"
+	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading env file")
+	}
+	secret := os.Getenv("SECRET")
+	fmt.Println(secret)
+
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 	r.Get("/", func(writer http.ResponseWriter, request *http.Request) {
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/ankurdubey28/atlas-platform
 
 go 1.25.8
 
-require github.com/go-chi/chi/v5 v5.2.5 // indirect
+require (
+	github.com/go-chi/chi/v5 v5.2.5 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+)
